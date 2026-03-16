c5ee1df9ad7fcf11faa9686d18291ec8b6138bae
JXPM <bilejohan04@gmail.com>
2026-03-16 10:11:15 +0100

    first commit

---
+++ Journal.sh
@@ -0,0 +1,8 @@
+# Créer un nouveau dépôt GitHub 
+git init
+git branch -M main
+git add .
+git commit -m "first commit"
+gh repo create eval_cdmia --private
+git remote add origin https://github.com/JXPM/eval_cdmia.git
+git push --set-upstream origin main
